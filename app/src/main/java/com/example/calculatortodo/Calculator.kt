a5ac1679d78703b9fc675ceff9f0028ccaf6ff2a
AliciaVegaMoreno <avegmor781@g.educaand.es>
2024-09-12 14:21:55 +0200

    Esqueleto de calculadora

---
+++ app/src/main/java/com/example/calculatortodo/Calculator.kt
@@ -0,0 +1,7 @@
+package com.example.calculatortodo
+
+/* Pon aquí el código para evaluar una expresión*/
+
+fun calculateResult(expression: String): String {
+    return ""
+}
--- app/src/main/java/com/example/calculatortodo/MainActivity.kt
+++ app/src/main/java/com/example/calculatortodo/MainActivity.kt
@@ -1,47 +1,40 @@
 package com.example.calculatortodo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculatortodo.ui.theme.CalculatorTODOTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CalculatorTODOTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    CalculatorScreen()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CalculatorScreen() {
+    /* Haz aqui la interfaz de la calculadora*/
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     CalculatorTODOTheme {
-        Greeting("Android")
+        CalculatorScreen()
     }
 }
